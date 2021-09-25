8a256d122fd3d930ef56cd30c856921cf511398f
EPCR00 <88967241+EPCR00@users.noreply.github.com>
2021-09-25 12:37:12 -0600

    Add files via upload

---
+++ 201902886_RENTAUTOS/201902886_RENTAUTOS/Module1.vb
@@ -0,0 +1,84 @@
+﻿Module Module1
+
+
+    Public Hondadia As Double = 250.0
+    Public Mercedesdia As Double = 450.0
+    Public Toyotadia As Double = 325.0
+    Public Mazdadia As Double = 300.0
+
+    Public indice As Byte = 0
+    Public diasalquiler As Integer
+    Public pagoparcial As Double
+
+    Public Nit(6) As String
+    Public Placa(6) As String
+    Public Marca(6) As String
+    Public Dias(6) As Integer
+    Public Parcial(6) As Double
+    Public Descuento(6) As Double
+    Public Total(6) As Double
+
+    Sub Limpiar_entradas()
+
+        Form1.combomarca.SelectedIndex = "-1"
+        Form1.txtnit.Clear()
+        Form1.txtplaca.Clear()
+        Form1.txtefectivo.Clear()
+        Form1.txttarjeta.Clear()
+        Form1.txtdias.Clear()
+        Form1.checkefectivo.Checked = "False"
+        Form1.CheckTarjeta.Checked = "False"
+        Form1.txtefectivo.Enabled = "False"
+        Form1.txttarjeta.Enabled = "False"
+
+    End Sub
+
+    Sub mostrar_resultado()
+
+        Form1.listnit.Items.Add(Nit(indice))
+        Form1.listplaca.Items.Add(Placa(indice))
+        Form1.listmarca.Items.Add(Marca(indice))
+        Form1.listdias.Items.Add(Dias(indice))
+        Form1.listparcial.Items.Add(Parcial(indice))
+        Form1.listdescuento.Items.Add(Descuento(indice))
+        Form1.listtotal.Items.Add(Total(indice))
+
+    End Sub
+
+    Sub Limpiar_Listas()
+
+        Form1.listnit.Items.Clear()
+        Form1.listplaca.Items.Clear()
+        Form1.listmarca.Items.Clear()
+        Form1.listdias.Items.Clear()
+        Form1.listparcial.Items.Clear()
+        Form1.listdescuento.Items.Clear()
+        Form1.listtotal.Items.Clear()
+
+    End Sub
+
+    Sub Limpiar_vectores()
+
+        Form1.listnit.Items.Clear()
+        Form1.listplaca.Items.Clear()
+        Form1.listmarca.Items.Clear()
+        Form1.listdias.Items.Clear()
+        Form1.listparcial.Items.Clear()
+        Form1.listdescuento.Items.Clear()
+        Form1.listtotal.Items.Clear()
+
+        indice = 0
+        MsgBox("Puede realizar ingresos nuevamente", vbExclamation)
+
+    End Sub
+
+
+
+
+
+
+
+
+
+
+End Module
